4ab9c07ee44ba2c86d5943e3fe8dcb6472157557
Blanca <bangelesr@toluca.tecnm.mx>
2022-05-07 23:28:45 -0500

    Login1

---
+++ Logincheck.php
@@ -0,0 +1,40 @@
+<?php
+
+$servidor = "localhost";
+$usuarioBD = "root";
+$pwdBD = "";
+$nomBD = "";
+
+
+$db = mysqli_connect($servidor, $usuarioBD, $pwdBD, $nomBD);
+
+$correo = $_GET["correo"];
+$password = $_GET["pass"];
+
+
+$sql = "SELECT id_usuario,password,rol,correo  from Usuarios where correo='" . $correo . "'";
+$result = mysqli_query($db, $sql);
+$numero_filas = mysqli_num_rows($result);
+
+if ($numero_filas == 0) {
+	echo "El usuario que ingresaste es incorrecto";
+	header("Location: login.php");
+	die();
+} else {
+
+	$mostrar = mysqli_fetch_array($result);
+	$rol = $mostrar['rol'];
+	$id_usuario = $mostrar['id_usuario'];
+}
+if ($password == $mostrar['password']) {
+	session_start();
+	$_SESSION["info"]["user"] = $correo;
+	$_SESSION["info"]["rol"] = $rol;
+	$_SESSION["id_usuario"] = $id_usuario;
+
+
+	header("Location: home.php");
+	die();
+} else {
+	echo "Contraseña incorrecta";
+}
