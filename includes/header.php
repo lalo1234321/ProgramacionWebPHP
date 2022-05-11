6f924752a3abb3639863eadb98ba924460df106a
ErikJaimes <86024852+ErikJaimes@users.noreply.github.com>
2022-05-10 20:41:59 -0500

    Pantallas Terminos y Acerca de

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <header>
     <div class="overlay">
+        <h1>CIENCIFICATE</h1>
         <h3>ARTÍCULOS SOBRE ÚLTIMOS DESCUBRIMIENTOS EN EL ÁREA DE LA FÍSICA EN EL SIGLO 20 Y 21</h3>
     </div>
 </header>
